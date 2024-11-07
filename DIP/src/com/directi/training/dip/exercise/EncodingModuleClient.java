dd7bc1d46c60de3d7b624fb775f9dcc2b32e937a
Rjibaa <rjibaahmed73@gmail.com>
2024-11-07 19:52:20 +0100

    exercices : Correction of the two exercices of SRP and DIP

--- DIP/src/com/directi/training/dip/exercise/EncodingModuleClient.java
+++ DIP/src/com/directi/training/dip/exercise/EncodingModuleClient.java
@@ -4,8 +4,8 @@ import java.io.IOException;
 
 public class EncodingModuleClient {
     public static void main(String[] args) throws IOException {
-        DatabaseInterface db = new MyDatabase();
-        EncodingModule encodingModule = new EncodingModule(db);
+        DatabaseInterface database = new MyDatabase();
+        EncodingModule encodingModule = new EncodingModule(database);
         encodingModule.encodeWithFiles();
         encodingModule.encodeBasedOnNetworkAndDatabase();
     }
